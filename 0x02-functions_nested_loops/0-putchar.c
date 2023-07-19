9ed2e36e60a2a2d98bf8862095e9285c42be4851
root <kariukikamau05@gmail.com>
2023-07-19 14:27:45 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,15 @@
-#include "stdio.h"
+#include <stdio.h>
 
 /**
  *main - Entry point
+ *
+ *This fubction prints "_putchar" followed by a new line
  *
  *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+printf("_putchar\n");
 return (0);
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,26 @@
-#include <stdio.h>
+#include "stdio.h"
+#include "main.h"
 
 /**
- * print_alphabet_x10 - Prints 10 times the alphabet in lowercase
- *
- * Return: void
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
  */
 void print_alphabet_x10(void)
 {
-int i;
-char c;
+	char ch;
+	int i;
 
-for (i = 0; i < 10; i++)
-{
-for (c = 'a'; c <= 'z'; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
