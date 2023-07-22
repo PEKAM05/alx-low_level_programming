7436022299a1a7b50fb87892b1d911a5beb8aed5
root <kariukikamau05@gmail.com>
2023-07-21 19:28:36 -0700

    update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,21 +6,21 @@
  */
 void print_numbers(int n)
 {
-	unsigned int n1;
+unsigned int n1;
 
-	if (n < 0)
-	{
-		n1 = -n;
-		putchar('-');
-	} else
-	{
-		n1 = n;
-	}
+if (n < 0)
+{
+n1 = -n;
+putchar('-');
+} else
+{
+n1 = n;
+}
 
-	if (n1 / 10)
-	{
-		print_numbers(n1 / 10);
-	}
+if (n1 / 10)
+{
+print_numbers(n1 / 10);
+}
 
-	putchar((n1 % 10) + '0');
+putchar((n1 % 10) + '0');
 }
